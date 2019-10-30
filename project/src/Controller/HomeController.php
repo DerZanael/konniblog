8f7979d645b830f8fcd4ae79c55f7691e53e9266
fdweb <Franois Deschamps>
2019-10-30 09:32:20 +0100

    Basic front + fontawesome

---
+++ project/src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Route("/home", name="home")
+     */
+    public function index(Request $request)
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
